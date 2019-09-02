0a72fcc9522305e1187e18b3cf7e3057c93114b6
HB <hb265419@126.com>
2019-09-02 14:27:12 +0800

    add user2 say nice to meet user1 too and MACRO PRINT

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,15 @@
 
 #include<iostream>
+#include "print.h"
 using namespace std;
 
 int main()
 {
+	PRINT
 	cout << "user1 say hello to user2." << endl;
-	cout << "user2 say hi to user1" << endl;
-	cout << "user1 say nice to meet user2" << endl;
+	cout << "user2 say hi to user1." << endl;
+	cout << "user1 say nice to meet user2." << endl;
+	cout << "user2 say nice to meet user1 too." << endl;
+	PRINT
 	return 0;
 }
---
+++ print.h
@@ -0,0 +1,4 @@
+
+#include<stdio.h>
+
+#define PRINT std::cout << "file--" << __FILE__ << "--line--" << __LINE__ << std::endl; 

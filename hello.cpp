e2db62ab664a96ac87bde895444e18f38526dd9d
hb <hb265419@126.com>
2019-09-02 14:37:54 +0800

    add user1 say thanks user2 for adding MACRO PRINT

--- hello.cpp
+++ hello.cpp
@@ -7,5 +7,6 @@ int main()
 	cout << "user1 say hello to user2." << endl;
 	cout << "user2 say hi to user1" << endl;
 	cout << "user1 say nice to meet user2" << endl;
+	cout << "user1 say thanks user2 for adding MACRO PRINT." << endl;
 	return 0;
 }
